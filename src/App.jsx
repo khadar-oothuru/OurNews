c191363c132d2edaf0aac76a7b86443909726782
khadar-oothuru <khadaroothuru@gmail.com>
2024-06-04 06:34:20 +0530

    all files recovered

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import QuesCards from "./Components/QuesCards";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-center"> hello </h1>
+
+      <QuesCards />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
